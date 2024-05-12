8be359158a4f685caa8e4ae6252bb5dc8e483b34
taokobik1234 <thienvovinpro123@gmail.com>
2024-05-12 10:53:30 +0700

    create button min cart

--- middlewares/client/cart.middlewares.js
+++ middlewares/client/cart.middlewares.js
@@ -11,7 +11,12 @@ module.exports.cartId = async (req,res,next) =>{
             expires: new Date(Date.now() + expiresTime)
         });
     } else {
+        const cart = await Cart.findOne({
+            _id: req.cookies.cartId
+        });
 
+        cart.totalQuantity = cart.products.reduce((sum,item)=> sum + item.quantity, 0);
+        res.locals.miniCart = cart;
     }
     next();
 }
